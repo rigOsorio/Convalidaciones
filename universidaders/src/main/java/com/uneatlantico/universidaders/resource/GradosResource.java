32919e83f183e25dbd3014c257818e99b1a5eae1
Miguel Cabezon <miguel.cabezon@alumnos.uneatlantico.es>
2019-03-12 15:24:08 +0100

    la sexta venida de jesus

--- universidaders/src/main/java/com/uneatlantico/universidaders/resource/GradosResource.java
+++ universidaders/src/main/java/com/uneatlantico/universidaders/resource/GradosResource.java
@@ -16,18 +16,16 @@ public class GradosResource {
 	GradosRepository gradosRepository;
 
 
-	
-<<<<<<< HEAD
-	@GetMapping("/carrers/{idUniversidad}")
-	public List<Grados> getGrados(@PathVariable("idUniversidad") final Integer idUniversidad){
 
-		List<Grados> grados=gradosRepository.findByidUniversidad(idUniversidad);
-        return grados;
-=======
+	@GetMapping("/carrers/{idUniversidad}")
+	public List<Grados> getGrados(@PathVariable("idUniversidad") final Integer idUniversidad) {
+
+		List<Grados> grados = gradosRepository.findByidUniversidad(idUniversidad);
+		return grados;
+	}
 	@GetMapping("/carreras/{id}")
 	public List<Grados> getGrados(@PathVariable(name = "id") final int idUniversidad){
 		return gradosRepository.findByidUniversidad(idUniversidad);
->>>>>>> 96941b387a4051530fd3762a3b57b3c4d9358867
 	}
 	
 	@GetMapping("/all")
