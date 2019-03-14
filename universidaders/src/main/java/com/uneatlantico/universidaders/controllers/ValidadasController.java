6aebdf61c837f86b2e26fb4a6b418f2f267db0f0
Jenkins Integration <leeeroy.jenkins.ci@gmail.com>
2019-03-14 14:37:56 +0100

    ya casi

--- universidaders/src/main/java/com/uneatlantico/universidaders/controllers/ValidadasController.java
+++ universidaders/src/main/java/com/uneatlantico/universidaders/controllers/ValidadasController.java
@@ -1,17 +1,27 @@
 package com.uneatlantico.universidaders.controllers;
 
+import com.uneatlantico.universidaders.model.Asignaturas;
+import com.uneatlantico.universidaders.resource.AsignaturasResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping(value = "views")
 public class ValidadasController {
+    @Autowired
+    AsignaturasResource asignaturasResource;
+
     @PostMapping(value = "validadas")
     public String valicacion(@RequestParam String idAsignatura, Model model){
-        model.addAttribute("ids",idAsignatura);
+        List<Asignaturas> asignaturas = asignaturasResource.getAprobadas(separarId(idAsignatura));
+        model.addAttribute("ids",asignaturas);
 
         model.addAttribute("title","Validacion");
 
@@ -21,5 +31,14 @@ public class ValidadasController {
     public String undefined(Model model){
 
         return "views/validadas";
+    }
+    public List<Integer> separarId(String lista){
+        String[] list;
+        List<Integer> listaId=new ArrayList<Integer>();
+        list=(lista.split(","));
+        for(int i=0;i<list.length;i++){
+            listaId.add(Integer.parseInt(list[i]));
+        }
+        return listaId;
     }
 }
