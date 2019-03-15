bfe59f367e31c25cf0b1a7968a4d1cbcde9f6493
Jenkins Integration <leeeroy.jenkins.ci@gmail.com>
2019-03-15 15:30:18 +0100

    algo

--- universidaders/src/main/java/com/uneatlantico/universidaders/resource/AsignaturasResource.java
+++ universidaders/src/main/java/com/uneatlantico/universidaders/resource/AsignaturasResource.java
@@ -34,11 +34,9 @@ public class AsignaturasResource {
     @PostMapping("/aprovadas")
     public List<Asignaturas> getAprobadas(@RequestParam("List<Integer>") List<Integer> lista) {
         List<Asignaturas> listaAporabadas=new ArrayList<Asignaturas>();
-        List<Asignaturas> listaEquivalencia=new ArrayList<Asignaturas>();//almacena una lista con las asignaturas que equivalen
         for(int i=0;i<lista.size();i++) {
             if(asignaturaRepository.findByid(lista.get(i)).getValidacion()==1) {
                 listaAporabadas.add(asignaturaRepository.findByid(lista.get(i)));
-                listaEquivalencia.add(asignaturaRepository.findByid(listaAporabadas.get(i).getIdAsigantura()));
             }
         }
         return listaAporabadas;
