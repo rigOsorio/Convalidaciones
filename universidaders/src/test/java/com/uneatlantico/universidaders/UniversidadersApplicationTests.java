f2123f0fd4aff778b6c40b172a146ef586ab5c2e
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2019-05-09 17:20:04 +0200

    Se agrego la dependencia hamcrest para test

--- universidaders/src/test/java/com/uneatlantico/universidaders/UniversidadersApplicationTests.java
+++ universidaders/src/test/java/com/uneatlantico/universidaders/UniversidadersApplicationTests.java
@@ -1,4 +1,4 @@
-package com.uneatlantico.universidaders;
+/*package com.uneatlantico.universidaders;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,3 +14,4 @@ public class UniversidadersApplicationTests {
 	}
 
 }
+*/
--- universidaders/src/test/java/com/uneatlantico/universidaders/controllers/UniversidadesControllerTest.java
+++ universidaders/src/test/java/com/uneatlantico/universidaders/controllers/UniversidadesControllerTest.java
@@ -1,19 +1,19 @@
 package com.uneatlantico.universidaders.controllers;
 
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
+import static org.hamcrest.Matchers.*;
 import com.uneatlantico.universidaders.resource.UniversidadesResource;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -36,7 +36,9 @@ public class UniversidadesControllerTest {
 	@Test
 	public void testIndex() throws Exception {
 		 mockMvc.perform(get("/views"))
-		 			.andExpect(status().isOk());
+		 			.andExpect(status().isOk())
+		 			.andExpect(view().name("views/index"))
+		 			.andExpect(model().attribute("title", "Universidades"));
 	}
 
 }
